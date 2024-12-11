bd5272d2e06d1416457389cc437d0a40dc18293c
Ahmed Nafea <>
2024-12-11 21:02:48 +0300

    update

--- docker/entrypoint.local.sh
+++ docker/entrypoint.local.sh
@@ -16,5 +16,5 @@ php artisan optimize clear
 php artisan view:clear
 php artisan route:clear
 
-php-fpm -D
-nginx -g "daemon off;"
+sudo php-fpm -D
+sudo nginx -g "daemon off;"
--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -44,5 +44,5 @@ chmod -R 775 /app/storage/framework/sessions
 chmod -R 775 /app/bootstrap
 chmod -R 775 /app/bootstrap/cache
 
-php-fpm -D
-nginx -g "daemon off;"
+sudo php-fpm -D
+sudo nginx -g "daemon off;"
